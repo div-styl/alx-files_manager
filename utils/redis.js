dd7e5982770ba50094b997712fdd042c580777c3
div-styl <gouhmada@gmail.com>
2024-03-10 13:01:34 +0000

    Update dependencies and add Redis client

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import redis from "redis";
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.on("error", (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+    this.client.on("connect", () => {
+      console.log("Redis client connected to the server");
+    });
+  }
+  isAlive() {
+    return this.client.connected;
+  }
+  async get(key) {
+    return this.getAsync(key);
+  }
+  async set(key, value, duration) {
+    this.client.setEx(key, duration, value);
+  }
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;

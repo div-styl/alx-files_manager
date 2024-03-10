014d693f07c4fd95f0a8d756b7bb4018f539e45e
div-styl <gouhmada@gmail.com>
2024-03-10 17:17:19 +0000

    Refactor RedisClient class and main.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,10 +1,12 @@
-import redis from "redis";
+import * as redis from "redis";
 import { promisify } from "util";
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
     this.client.on("error", (error) => {
       console.log(`Redis client not connected to the server: ${error.message}`);
     });
@@ -16,13 +18,14 @@ class RedisClient {
     return this.client.connected;
   }
   async get(key) {
-    return this.getAsync(key);
+    return await this.getAsync(key);
   }
   async set(key, value, duration) {
-    this.client.setEx(key, duration, value);
+    await this.setAsync(key, value);
+    await this.client.expire(key, duration);
   }
   async del(key) {
-    this.client.del(key);
+    this.client.delAsync(key);
   }
 }
 

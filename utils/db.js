e874af4fdd4bb9991924ceedf83d5cd51c642ba9
div-styl <gouhmada@gmail.com>
2024-03-11 10:17:48 +0000

    eslint updated

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,9 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 // db variables
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || "files_manager";
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const URL = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
@@ -11,8 +11,8 @@ class DBClient {
     MongoClient.connect(URL, { useUnifiedTopology: true }, (err, client) => {
       if (!err) {
         this.db = client.db(DB_DATABASE);
-        this.usersCollection = this.db.collection("users");
-        this.filesCollection = this.db.collection("files");
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
       } else {
         console.log(err.message);
         this.db = false;
--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,5 @@
-import * as redis from "redis";
-import { promisify } from "util";
+import * as redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
@@ -7,23 +7,27 @@ class RedisClient {
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.setAsync = promisify(this.client.set).bind(this.client);
     this.delAsync = promisify(this.client.del).bind(this.client);
-    this.client.on("error", (error) => {
+    this.client.on('error', (error) => {
       console.log(`Redis client not connected to the server: ${error.message}`);
     });
-    // this.client.on("connect", () => {
-    //   console.log("Redis client connected to the server");
+    // this.client.on('connect', () => {
+    //   console.log('Redis client connected to the server');
     // });
   }
+
   isAlive() {
     return this.client.connected;
   }
+
   async get(key) {
-    return await this.getAsync(key);
+    return this.getAsync(key);
   }
+
   async set(key, value, duration) {
     await this.setAsync(key, value);
     await this.client.expire(key, duration);
   }
+
   async del(key) {
     this.client.delAsync(key);
   }

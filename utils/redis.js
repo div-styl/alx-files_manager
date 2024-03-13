e0d99bc4600d0b19d173cadc2f54347ea976c3e4
adilmae <itsadilhadid@gmail.com>
2024-03-13 03:04:04 +0000

    removing pnpm

--- utils/redis.js
+++ utils/redis.js
@@ -3,32 +3,52 @@ import { promisify } from "util";
 
 class RedisClient {
   constructor() {
+    this.connected = true;
     this.client = redis.createClient();
-    this.getAsync = promisify(this.client.get).bind(this.client);
-
-    this.client.on("error", (error) => {
-      console.log(`Redis client not connected to the server: ${error}`);
+    this.client.on("error", (err) => {
+      this.connected = false;
+      console.log(`Redis client not connected to the server: ${err}`);
     });
+    this.client.on("connect", () => {
+      this.connected = true;
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client.connected;
+    return this.connected;
   }
 
   async get(key) {
-    const value = await this.getAsync(key);
+    let value = null;
+    try {
+      value = await this.getAsync(key);
+    } catch (err) {
+      console.log(err);
+    }
     return value;
   }
 
   async set(key, value, duration) {
-    this.client.set(key, value);
-    this.client.expire(key, duration);
+    try {
+      await this.setAsync(key, duration, value);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async del(key) {
-    this.client.del(key);
+    try {
+      await this.delAsync(key);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
 const redisClient = new RedisClient();
+
 export default redisClient;

20529292afc2e05b147496fdfffeedb25a70ff0e
adilmae <itsadilhadid@gmail.com>
2024-03-13 03:00:51 +0000

    removing pnpm

--- utils/redis.js
+++ utils/redis.js
@@ -9,37 +9,23 @@ class RedisClient {
     this.client.on("error", (error) => {
       console.log(`Redis client not connected to the server: ${error}`);
     });
-
-    this.client.on("connect", () => {
-      console.log("Redis client connected to the server");
-    });
   }
 
   isAlive() {
     return this.client.connected;
   }
 
-  waitForConnect() {
-    return new Promise((resolve, reject) => {
-      this.client.on("connect", resolve);
-      this.client.on("error", reject);
-    });
-  }
-
   async get(key) {
-    await this.waitForConnect();
     const value = await this.getAsync(key);
     return value;
   }
 
   async set(key, value, duration) {
-    await this.waitForConnect();
     this.client.set(key, value);
     this.client.expire(key, duration);
   }
 
   async del(key) {
-    await this.waitForConnect();
     this.client.del(key);
   }
 }

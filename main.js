ef61d058994dea0a6a7cb4c775904a2052336e3a
adilmae <itsadilhadid@gmail.com>
2024-03-13 02:35:09 +0000

    fixing task 0

--- main.js
+++ main.js
@@ -1,54 +1,12 @@
-import redis from "redis";
-import { promisify } from "util";
+import redisClient from "./utils/redis";
 
-class RedisClient {
-  constructor() {
-    this.connected = true;
-    this.client = redis.createClient();
-    this.client.on("error", (err) => {
-      this.connected = false;
-      console.log(`Redis client not connected to the server: ${err}`);
-    });
-    this.client.on("connect", () => {
-      this.connected = true;
-    });
+(async () => {
+  console.log(redisClient.isAlive());
+  console.log(await redisClient.get("myKey"));
+  await redisClient.set("myKey", 12, 5);
+  console.log(await redisClient.get("myKey"));
 
-    this.getAsync = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.setex).bind(this.client);
-    this.delAsync = promisify(this.client.del).bind(this.client);
-  }
-
-  isAlive() {
-    return this.connected;
-  }
-
-  async get(key) {
-    let value = null;
-    try {
-      value = await this.getAsync(key);
-    } catch (err) {
-      console.log(err);
-    }
-    return value;
-  }
-
-  async set(key, value, duration) {
-    try {
-      await this.setAsync(key, duration, value);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  async del(key) {
-    try {
-      await this.delAsync(key);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-}
-
-const redisClient = new RedisClient();
-
-export default redisClient;
+  setTimeout(async () => {
+    console.log(await redisClient.get("myKey"));
+  }, 1000 * 10);
+})();
